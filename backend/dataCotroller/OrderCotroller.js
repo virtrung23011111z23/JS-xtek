51afc5df2b3ea2c754d2f8d49195caf041a7ef86
Nguyễn Tiến Trung <tjentrung8tz@gmail.com>
2025-09-18 19:28:50 +0700

    first

--- backend/dataCotroller/OrderCotroller.js
+++ backend/dataCotroller/OrderCotroller.js
@@ -7,7 +7,9 @@ export function postOrder(req,res) {
 }
 export function removeOrder(req, res) {
   const { id } = req.params;
+  const prevCount = orders.length;
   orders = orders.filter(item => String(item.idOrder) !== String(id));
+  return res.status(200).json({ success: true, orders });
 }
 export function getOrders(req, res) {
   return res.json(orders);
